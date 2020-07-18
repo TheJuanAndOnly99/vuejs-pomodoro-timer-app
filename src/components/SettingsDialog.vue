43a839abcd3c493774e836bdc5620d3581bcbd8f
Juan Estrella <juan.estrella137@gmail.com>
2020-07-18 13:04:35 +0200

    Create a settings component

---
+++ src/components/SettingsDialog.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="primary" dark v-bind="attrs" v-on="on">Open Dialog</v-btn>
+    </template>
+    <v-card></v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: {
+      type: Boolean,
+      require: true
+    }
+  }
+};
+</script>
