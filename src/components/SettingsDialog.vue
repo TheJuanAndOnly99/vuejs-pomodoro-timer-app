b78bb751a768b3d2aa36edd1692cf30647919738
Juan Estrella <juan.estrella137@gmail.com>
2020-07-18 13:40:29 +0200

    Export SettingsDialog component

--- src/components/SettingsDialog.vue
+++ src/components/SettingsDialog.vue
@@ -1,9 +1,12 @@
 <template>
   <v-dialog v-model="dialog" persistent max-width="600px">
-    <template v-slot:activator="{ on, attrs }">
-      <v-btn color="primary" dark v-bind="attrs" v-on="on">Open Dialog</v-btn>
-    </template>
-    <v-card></v-card>
+    <v-card>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+        <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-dialog>
 </template>
 
