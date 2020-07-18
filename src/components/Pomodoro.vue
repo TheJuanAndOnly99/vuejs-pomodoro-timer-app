4e42108d49c02334e83ba711bbfa93cdd83ac8d9
Juan Estrella <juan.estrella137@gmail.com>
2020-07-18 09:54:38 +0200

    Add buttons to pomodoro clocl

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -2,6 +2,16 @@
   <v-card class="mt-8">
     <v-tabs v-model="timerType">
       <v-tab v-for="tab in tabTitles" :key="tab">{{ tab }}</v-tab>
+      <v-tabs-items v-model="timerType">
+        <v-tab-item>
+          <v-card color="basil" class="pa-5" flat>
+            <h1 class="time">00:00</h1>
+            <v-btn color="primary">Start</v-btn>
+            <v-btn>Stop</v-btn>
+            <v-btn>Reset</v-btn>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
     </v-tabs>
   </v-card>
 </template>
