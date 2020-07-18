b273cb017a2d19e1f2e2d651d58a0a45c177116d
Juan Estrella <juan.estrella137@gmail.com>
2020-07-18 09:15:29 +0200

    Create Pomodoro component

---
+++ src/components/Pomodoro.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card>
+    <v-tabs v-model="timerType">
+      <v-tab v-for="tab in tabTitles" :key="tab"></v-tab>
+    </v-tabs>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      timerType: 0,
+      tabsTitles: ["Pomodoro", "Short Break", "Long Break"]
+    };
+  }
+};
+</script>
