71cf084f319d34b1b495373359505300f2c71ec6
Juan Estrella <juan.estrella137@gmail.com>
2020-07-18 10:26:24 +0200

    Create start, stop, and reset methods

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -4,17 +4,19 @@
       <v-tab v-for="tab in tabTitles" :key="tab">{{ tab }}</v-tab>
       <v-tabs-items v-model="timerType">
         <v-tab-item>
-          <v-card color="basil" class="pa-5" flat>
+          <v-card color="basil" class="pa-5 d-flex flex-column align-center" flat>
             <h1 class="time">00:00</h1>
-            <v-btn color="success">
-              <v-icon left>mdi-play-circle-outline</v-icon>Start
-            </v-btn>
-            <v-btn color="error">
-              <v-icon left>mdi-stop-circle-outline</v-icon>Stop
-            </v-btn>
-            <v-btn color="primary">
-              <v-icon left>mdi-restart</v-icon>Reset
-            </v-btn>
+            <div class="button-group">
+              <v-btn color="success">
+                <v-icon left>mdi-play-circle-outline</v-icon>Start
+              </v-btn>
+              <v-btn color="error">
+                <v-icon left>mdi-stop-circle-outline</v-icon>Stop
+              </v-btn>
+              <v-btn color="primary">
+                <v-icon left>mdi-restart</v-icon>Reset
+              </v-btn>
+            </div>
           </v-card>
         </v-tab-item>
       </v-tabs-items>
@@ -26,9 +28,21 @@
 export default {
   data() {
     return {
+      display: {
+        minutes: "00",
+        seconds: "00"
+      },
+      totalSeconds: 25 * 60,
       timerType: 0,
       tabTitles: ["Pomodoro", "Short Break", "Long Break"]
     };
+  },
+  methods: {
+    start() {
+      setInterval(() => {}, 1000);
+    },
+    stop() {},
+    reset() {}
   }
 };
 </script>
@@ -36,6 +50,8 @@ export default {
 <style lang="sass" scoped>
 .v-card
   width: 600px
+.v-btn
+  margin: 0 6px
 .time
   font-size: 80px
   font-weight: 400
